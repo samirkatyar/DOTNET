0a6f91681baa0de47eb5a3beaa2bf935667d7e53
Katyar Samir <43346830+samirkatyar@users.noreply.github.com>
2019-03-29 23:01:46 +0530

    Add files via upload

---
+++ NET/Practical-3/ConstructorOverloading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace p2
+{
+    public class Student
+    {
+        string name, enrollment, branch;
+        public Student(string name) {
+            this.name = name;
+        }
+        public Student(string name, string enrollment) {
+            this.name = name;
+            this.enrollment = enrollment;
+        }
+        public Student(string name, string enrollment, string branch) {
+            this.name = name;
+            this.enrollment = enrollment;
+            this.branch = branch;
+        }
+        public static void Main(String[] ar) {
+            Student s1 = new Student("samir");
+            Student s2 = new Student("samir","170473107010");
+            Student s3 = new Student("samir","170473107010","Computer");
+        }
+    }
+}
